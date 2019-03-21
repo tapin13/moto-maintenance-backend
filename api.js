af6ea6a7de7a7ed5db8f5b7fd320a27b2bed7e16
tapin13 <evgenyr@centerity.com>
2019-03-21 17:21:15 +0200

    tasks

--- api.js
+++ api.js
@@ -2,6 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
+const sqlite3 = require('sqlite3');
+
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -9,18 +11,28 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors('*'));
 
+const users = require('./users.json');
+
+const db = new sqlite3.Database('./db.sqlite3', (err) => {
+    if(err) {
+        console.log(err);
+    }
+    console.log(`connect to db - success`);
+
+    db.run('CREATE TABLE IF NOT EXISTS `tasks` (`title` TEXT, `distance` INTEGER, `price` REAL)', [], (err) => {
+        if(err) {
+            console.log(err);
+        }
+        console.log(`db CREATE TABLE - success`);
+    });
+});
+
 // app.get('/api/hello', (req, res) => {
 //   res.send({ express: 'Hello From Express' });
 // });
 
-let users = [
-    { username: 'admin', password: 'admin' },
-    { username: 'user', password: 'user' },
-    { username: 'papa', password: 'papa1' },
-];
-
 app.post('/api/login', (request, response) => {
-    //console.log(req.body);
+    console.log(request.body);
     let requestObject = request.body;
     // requestObject = JSON.parse(req.json);
     console.log(requestObject.username);
@@ -44,6 +56,44 @@ app.post('/api/login', (request, response) => {
     }
 
     response.send(JSON.stringify(responseObject));
+});
+
+app.post('/api/tasks', (request, response) => {
+    console.log(request.body);
+    responseObject = {
+        status: 400
+    };
+
+    db.all('SELECT * FROM `tasks`', [],(err, rows) => {
+        if(err) {
+            console.log(err);
+        }
+        console.log(`db SELECT - success`, rows);
+        responseObject.status = 200;
+        responseObject.data = rows;
+        response.send(JSON.stringify(responseObject));
+    });
+});
+
+app.post('/api/task/add', (request, response) => {
+    console.log(request.body);
+    responseObject = {
+        status: 400
+    };
+
+    db.run(
+        'INSERT INTO `tasks` (`title`, `distance`, `price`) VALUES (?,?,?)',
+        [ request.body.title, request.body.distance, request.body.price ],
+        (err) => {
+            if(err) {
+                console.log(err);
+            }
+            console.log(`db INSERT INTO - success`);
+
+            responseObject.status = 200;
+            response.send(JSON.stringify(responseObject));
+        }
+    );
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
